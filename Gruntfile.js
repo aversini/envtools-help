97b19cfc5aea38d5d1606eaa2bf525cbd2ddb0d1
Arno V <versini@gmail.com>
2016-12-06 17:33:28 -0800

    inlining the Envtools logo (the text)

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,12 +60,12 @@ module.exports = function (grunt) {
           dest: 'tmp/help.html'
         }]
       },
-      // images: {
-      //   files: [{
-      //     src: ['data/assets/images/**/*'],
-      //     dest: 'docs/'
-      //   }]
-      // },
+      images: {
+        files: [{
+          src: ['docs/data/assets/images/envtools-retina.png'],
+          dest: 'tmp/envtools-retina.png'
+        }]
+      },
       svg: {
         files: [{
           src: ['data/templates/help/envtools-svg-symbols.html'],
